ab539f46691b937d64bcd0c4014e352e89fc892e
Maalik-ghub <nabeellatheef9@gmail.com>
2026-02-20 00:50:57 +0530

    intro and image optimisations

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { useState } from 'react';
 import MainPage from './pageComponents/mainPage';
+import LoadingPage from './pageComponents/loadingPage';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
   return (
     <div className="App">
+      {isLoading?
+      <LoadingPage setIsLoading={setIsLoading}></LoadingPage>
+      :
       <MainPage></MainPage>
+      }
     </div>
   );
 }
--- src/pageComponents/loadingPage.jsx
+++ src/pageComponents/loadingPage.jsx
@@ -1,6 +1,16 @@
-const LoadingPage = () => {
+import styles from "../css/loadingPage.module.css";
+import loading from "../css/images/loading.mp4";
+
+const LoadingPage = ({setIsLoading}) => {
+    function handleEnd() {
+        setIsLoading(false)
+    }
     return(
-        <></>
+        <div className={styles.loadingPage}>
+            <video autoPlay playsInline onEnded={handleEnd} muted>
+                <source src={loading}></source>
+            </video>
+        </div>
     )
 }
 
